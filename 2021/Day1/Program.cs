2a081b2d6a3a8005ef1cdce812364252c6849900
Robin Granqvist <robgradev@gmail.com>
2021-12-01 09:37:59 +0100

    Day 1

---
+++ 2021/Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿var numbers = File.ReadAllLines("input.txt").Select(line => Int32.Parse(line)).ToList();
+
+void PartOne()
+{
+    int previousNumber = 9999;
+    int increased = 0;
+
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        if (numbers[i] > previousNumber)
+            increased++;
+
+        previousNumber = numbers[i];
+    }
+    Console.WriteLine("Increased: " + increased);
+}
+
+void PartTwo()
+{
+    int previousSum = 99999;
+    int increased = 0;
+
+    for (int i = 0; i < numbers.Count - 2; i++)
+    {
+        int firstNumber = numbers[i];
+        int secondNumber = numbers[i + 1];
+        int thirdNumber = numbers[i + 2];
+        int sum = firstNumber + secondNumber + thirdNumber;
+        if (sum > previousSum)
+            increased++;
+        previousSum = sum;
+    }
+    Console.WriteLine("Increased: " + increased);
+}
+
+PartTwo();
